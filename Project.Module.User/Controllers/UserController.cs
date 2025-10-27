5d790c27598f21e88597946df840e3771fb2e528
Administrator <Administrator@LAPTOP-36J9P2RT>
2025-10-27 14:55:40 +0300

    user controllers added

---
+++ Project.Module.User/Controllers/UserController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Ettad.CrossCutting.Common.Security;
+using Ettad.ResponseHandler.Models;
+using Ettad.User.Services.DTO;
+using Ettad.User.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ettad.User.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ApiControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("{id}")]
+        [CheckAuthorize("Permissions.SystemUsers.View", "Permissions.SystemUsers.page")]
+
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _userService.GetByIdAsync(id);
+            return ProcessResponse(response);
+        }
+
+        [HttpGet]
+        [CheckAuthorize("Permissions.SystemUsers.View", "Permissions.SystemUsers.page")]
+
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _userService.GetAllAsync();
+            return ProcessResponse(response);
+        }
+
+        [CheckAuthorize("Permissions.SystemUsers.Create")]
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateUserDto dto)
+        {
+            var response = await _userService.CreateAsync(dto);
+            return ProcessResponse(response);
+        }
+
+        [CheckAuthorize(
+"Permissions.SystemUsers.Edit"
+
+)]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(string id, [FromBody] UpdateUserDto dto)
+        {
+            dto.Id = id;
+            var response = await _userService.UpdateAsync(dto);
+            return ProcessResponse(response);
+        }
+        [CheckAuthorize(
+"Permissions.SystemUsers.Delete"
+
+)]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var response = await _userService.DeleteAsync(id);
+            return ProcessResponse(response);
+        }
+
+        [HttpGet("{id}/roles")]
+        //[CheckAuthorize("Permissions.SystemUsers.AssignRoles")] 
+        public async Task<IActionResult> GetUserRoles(string id)
+        {
+            var response = await _userService.GetUserRolesAsync(id);
+            return ProcessResponse(response);
+        }
+
+        [HttpPut("{id}/roles")]
+        //[CheckAuthorize("Permissions.SystemUsers.AssignRoles")] 
+        public async Task<IActionResult> UpdateUserRoles(string id, [FromBody] UpdateUserRolesDto dto)
+        {
+            var response = await _userService.UpdateUserRolesAsync(id, dto);
+            return ProcessResponse(response);
+        }
+    }
+
+}
