59cc508aaa1cef033f277821ff1b12937ee2db0d
Mohammad Jawabreh <issam.tech@floragroup.net>
2025-10-29 16:17:15 +0300

    feat: create lookup APIs

--- Project.EntityFramework/Utiliies/MainEntities.cs
+++ Project.EntityFramework/Utiliies/MainEntities.cs
@@ -89,6 +89,147 @@ public static class MainEntities
         CrudOperation.Edit,
         CrudOperation.Delete,
     };
+
+    [Category(CrudPermissionsGenerator.OrganizationSettings)]
+    public static readonly CrudOperation[] Propellants = new[]
+    {
+        CrudOperation.Page,
+        CrudOperation.View,
+        CrudOperation.Create,
+        CrudOperation.Edit,
+        CrudOperation.Delete,
+    };
+
+    [Category(CrudPermissionsGenerator.OrganizationSettings)]
+    public static readonly CrudOperation[] Units = new[]
+    {
+        CrudOperation.Page,
+        CrudOperation.View,
+        CrudOperation.Create,
+        CrudOperation.Edit,
+        CrudOperation.Delete,
+    };
+
+    [Category(CrudPermissionsGenerator.OrganizationSettings)]
+    public static readonly CrudOperation[] ProjectailMaterials = new[]
+    {
+        CrudOperation.Page,
+        CrudOperation.View,
+        CrudOperation.Create,
+        CrudOperation.Edit,
+        CrudOperation.Delete,
+    };
+
+    [Category(CrudPermissionsGenerator.OrganizationSettings)]
+    public static readonly CrudOperation[] NatureOptions = new[]
+    {
+        CrudOperation.Page,
+        CrudOperation.View,
+        CrudOperation.Create,
+        CrudOperation.Edit,
+        CrudOperation.Delete,
+    };
+
+    [Category(CrudPermissionsGenerator.OrganizationSettings)]
+    public static readonly CrudOperation[] Nsns = new[]
+    {
+        CrudOperation.Page,
+        CrudOperation.View,
+        CrudOperation.Create,
+        CrudOperation.Edit,
+        CrudOperation.Delete,
+    };
+
+    [Category(CrudPermissionsGenerator.OrganizationSettings)]
+    public static readonly CrudOperation[] PrimaryPurposes = new[]
+    {
+        CrudOperation.Page,
+        CrudOperation.View,
+        CrudOperation.Create,
+        CrudOperation.Edit,
+        CrudOperation.Delete,
+    };
+
+    [Category(CrudPermissionsGenerator.OrganizationSettings)]
+    public static readonly CrudOperation[] Manufacturers = new[]
+    {
+        CrudOperation.Page,
+        CrudOperation.View,
+        CrudOperation.Create,
+        CrudOperation.Edit,
+        CrudOperation.Delete,
+    };
+
+    [Category(CrudPermissionsGenerator.OrganizationSettings)]
+    public static readonly CrudOperation[] Hccs = new[]
+    {
+        CrudOperation.Page,
+        CrudOperation.View,
+        CrudOperation.Create,
+        CrudOperation.Edit,
+        CrudOperation.Delete,
+    };
+
+    [Category(CrudPermissionsGenerator.OrganizationSettings)]
+    public static readonly CrudOperation[] Depots = new[]
+    {
+        CrudOperation.Page,
+        CrudOperation.View,
+        CrudOperation.Create,
+        CrudOperation.Edit,
+        CrudOperation.Delete,
+    };
+
+    [Category(CrudPermissionsGenerator.OrganizationSettings)]
+    public static readonly CrudOperation[] HazardDivisions = new[]
+    {
+        CrudOperation.Page,
+        CrudOperation.View,
+        CrudOperation.Create,
+        CrudOperation.Edit,
+        CrudOperation.Delete,
+    };
+
+    [Category(CrudPermissionsGenerator.OrganizationSettings)]
+    public static readonly CrudOperation[] Countries = new[]
+    {
+        CrudOperation.Page,
+        CrudOperation.View,
+        CrudOperation.Create,
+        CrudOperation.Edit,
+        CrudOperation.Delete,
+    };
+
+    [Category(CrudPermissionsGenerator.OrganizationSettings)]
+    public static readonly CrudOperation[] CaseTypes = new[]
+    {
+        CrudOperation.Page,
+        CrudOperation.View,
+        CrudOperation.Create,
+        CrudOperation.Edit,
+        CrudOperation.Delete,
+    };
+
+    [Category(CrudPermissionsGenerator.OrganizationSettings)]
+    public static readonly CrudOperation[] Compatibilities = new[]
+    {
+        CrudOperation.Page,
+        CrudOperation.View,
+        CrudOperation.Create,
+        CrudOperation.Edit,
+        CrudOperation.Delete,
+    };
+
+    [Category(CrudPermissionsGenerator.OrganizationSettings)]
+    public static readonly CrudOperation[] Colors = new[]
+    {
+        CrudOperation.Page,
+        CrudOperation.View,
+        CrudOperation.Create,
+        CrudOperation.Edit,
+        CrudOperation.Delete,
+    };
+
     [Category(CrudPermissionsGenerator.OrganizationSettings)]
     public static readonly CrudOperation[] Organizations = new[]
 {
---
+++ Project.Module.Logic/Dtos/CountryDto.cs
@@ -0,0 +1,11 @@
+namespace Ettad.Module.lookup.Dtos
+{
+    public class CountryDto
+    {
+        public string NameAr { get; set; }
+        public string NameEn { get; set; }
+        public string Code { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
+
