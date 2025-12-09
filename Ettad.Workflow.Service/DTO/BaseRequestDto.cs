fd18fdfcaa38936462a40b1be17149c8881754e2
Mohammad Jawabreh <issam.tech@floragroup.net>
2025-12-09 12:29:38 +0300

    tracking current approver

--- Ettad.Workflow.Service/DTO/BaseRequestDto.cs
+++ Ettad.Workflow.Service/DTO/BaseRequestDto.cs
@@ -1,7 +1,10 @@
-using Ettad.Data.Enums;
 using Ettad.CrossCutting.Comman.FileUpload;
+using Ettad.Data.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Ettad.Workflows.Service.DTO
 {
@@ -10,20 +13,42 @@ namespace Ettad.Workflows.Service.DTO
         public long Id { get; set; }
         public string RequestNo { get; set; }
         public RequestType RequestType { get; set; }
-        public string Reason { get; set; }
+        public string? Reason { get; set; }
         public RequestPriority Priority { get; set; }
         public RequestStatus Status { get; set; }
-        public DateTime RequestDate { get; set; }
         public string? Notes { get; set; }
+
         public long DepartmentId { get; set; }
-        public string? RequesterId { get; set; }
+        public string RequesterId { get; set; }
         public long RequestPurposeId { get; set; }
+        public DateTime RequestDate { get; set; }
+
+        // Additional info
         public string? DepartmentName { get; set; }
+        public string? DepartmentNameAr { get; set; }
+        public string? DepartmentNameEn { get; set; }
         public string? RequesterName { get; set; }
         public string? RequesterUserName { get; set; }
         public string? RequestPurposeName { get; set; }
-        public List<FileUploadDto> Files { get; set; } = new List<FileUploadDto>();
+        public string? RequestPurposeNameAr { get; set; }
+        public string? RequestPurposeNameEn { get; set; }
+
+        // Lists
+        public List<object> RequestItems { get; set; } = new List<object>();
         public List<ApprovalHistoryDto> ApprovalHistory { get; set; } = new List<ApprovalHistoryDto>();
+        
+        // Files attached to the request (Order/Return/Discard)
+        public List<FileUploadDto> Files { get; set; } = new List<FileUploadDto>();
+        
+        // Usage related fields
+        public string? UsageLocation { get; set; }
+        public string? UsagePurpose { get; set; }
+        public DateTime? UsageDateFrom { get; set; }
+        public string? UsageTimeFrom { get; set; }
+        public DateTime? UsageDateTo { get; set; }
+        public string? UsageTimeTo { get; set; }
+        public int? NumberOfOfficer { get; set; }
+        public int? NumberOfOtherRank { get; set; }
       
     }
 
@@ -43,7 +68,7 @@ namespace Ettad.Workflows.Service.DTO
         public bool RequireHigherApproval { get; set; }
         public string? HigherApprovalRoleId { get; set; }
         public bool IsPending { get; set; } // True if this step hasn't been completed yet
+        public bool IsCurrentUserApprover { get; set; } // True if current user can approve this step
         public List<FileUploadDto> Files { get; set; } = new List<FileUploadDto>();
     }
 }
-
