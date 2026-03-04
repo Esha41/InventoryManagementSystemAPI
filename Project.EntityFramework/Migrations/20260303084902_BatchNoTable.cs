a3cf23a1b45526ed11599048a7f650a618354417
Abdalla Ahmed <abdalla.tech@floragroup.net>
2026-03-04 10:41:32 +0300

    chnage patch to Batch

--- Project.EntityFramework/Migrations/20260303084902_BatchNoTable.cs
+++ Project.EntityFramework/Migrations/20260303084902_BatchNoTable.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Ettad.EntityFramework.Migrations
 {
     /// <inheritdoc />
-    public partial class PatchNoTable : Migration
+    public partial class BatchNoTable : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
