a0866470953801c617fc7420ee1da373ab7fe4b2
ftomy <ftomy@LAPTOP-36J9P2RT>
2026-03-25 16:04:38 +0300

    ItemDepartmentAssignment table updated(remove soft delete)

--- Ettad.Inventory.Services/Explosives/ExplosiveService.cs
+++ Ettad.Inventory.Services/Explosives/ExplosiveService.cs
@@ -429,7 +429,7 @@ namespace Ettad.Inventory.Service.Explosives
         private async Task<bool> ItemHasReferencesAsync(long itemId)
         {
             var hasDeptAssignment = await _context.ItemDepartmentAssignments
-                .AnyAsync(x => x.ItemId == itemId && !x.IsDeleted);
+                .AnyAsync(x => x.ItemId == itemId);
             if (hasDeptAssignment) return true;
 
             var hasInventory = await _context.InventoryDetails
