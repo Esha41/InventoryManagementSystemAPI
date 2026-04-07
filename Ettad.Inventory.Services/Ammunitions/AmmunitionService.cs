844fb39ecbe0b457dc686fe4c922da118a7f2406
ftomy <ftomy@LAPTOP-36J9P2RT>
2026-04-07 16:34:10 +0300

    Revert "permanently delete updated"
    This reverts commit d89e67a50022017b9557af33d5f6456b6028993d.

--- Ettad.Inventory.Services/Ammunitions/AmmunitionService.cs
+++ Ettad.Inventory.Services/Ammunitions/AmmunitionService.cs
@@ -494,9 +494,6 @@ namespace Ettad.Inventory.Service.Ammunitions
                 await using var transaction = await _context.Database.BeginTransactionAsync();
                 try
                 {
-                    await _context.Database.ExecuteSqlRawAsync(
-                        "DELETE FROM BaseItemPrimaryPurposes WHERE BaseItemId = {0}", id);
-
                     var ammoDeleted = await _context.Database.ExecuteSqlRawAsync(
                         "DELETE FROM Ammunitions WHERE Id = {0}", id);
 
