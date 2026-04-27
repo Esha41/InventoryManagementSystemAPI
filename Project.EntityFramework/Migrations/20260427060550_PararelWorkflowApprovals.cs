afe9da64211e39cbecbd76b2192c11f1d5371eaa
Abdalla Ahmed <abdalla.tech@floragroup.net>
2026-04-27 10:25:15 +0300

    clean migrations

---
+++ Project.EntityFramework/Migrations/20260427060550_PararelWorkflowApprovals.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ettad.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class PararelWorkflowApprovals : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ChangedByRoleId",
+                table: "WorkflowStepApprovalLog",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "TargetRequestId",
+                table: "WorkflowApprovalSteps",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDelegation",
+                table: "WorkflowApprovalSteps",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApproverRoleId",
+                table: "WorkflowApprovalSteps",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "WorkflowStepParallelRoles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WorkflowStepId = table.Column<int>(type: "int", nullable: false),
+                    RoleId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModificationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkflowStepParallelRoles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkflowStepParallelRoles_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_WorkflowStepParallelRoles_WorkflowSteps_WorkflowStepId",
+                        column: x => x.WorkflowStepId,
+                        principalTable: "WorkflowSteps",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkflowStepParallelRoles_RoleId",
+                table: "WorkflowStepParallelRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkflowStepParallelRoles_WorkflowStepId",
+                table: "WorkflowStepParallelRoles",
+                column: "WorkflowStepId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkflowStepParallelRoles_WorkflowStepId_RoleId",
+                table: "WorkflowStepParallelRoles",
+                columns: new[] { "WorkflowStepId", "RoleId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkflowStepParallelRoles");
+
+            migrationBuilder.DropColumn(
+                name: "ChangedByRoleId",
+                table: "WorkflowStepApprovalLog");
+
+            migrationBuilder.DropColumn(
+                name: "ApproverRoleId",
+                table: "WorkflowApprovalSteps");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TargetRequestId",
+                table: "WorkflowApprovalSteps",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IsDelegation",
+                table: "WorkflowApprovalSteps",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+        }
+    }
+}
