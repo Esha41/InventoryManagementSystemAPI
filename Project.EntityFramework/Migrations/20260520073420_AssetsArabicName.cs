6c378b49cd50a5542d2b096664e48f3088b7016d
Abdalla Ahmed <abdalla.tech@floragroup.net>
2026-05-20 10:58:25 +0300

    add clean migration

---
+++ Project.EntityFramework/Migrations/20260520073420_AssetsArabicName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ettad.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class AssetsArabicName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NameAr",
+                table: "BaseItems",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NameAr",
+                table: "BaseItems");
+        }
+    }
+}
