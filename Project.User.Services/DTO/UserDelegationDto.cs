bc97c96e02e320bf9212db85b069a153be540218
Abdalla Ahmed <abdalla.tech@floragroup.net>
2026-06-02 15:19:42 +0300

    Enhance UserDelegationDto and UserDelegationService with additional fields for delegator and delegatee
    - Added properties for DelegatorFullNameEn, DelegatorFullNameAr, DelegatorMilitaryId, DelegateeFullNameEn, DelegateeFullNameAr, and DelegateeMilitaryId to UserDelegationDto.
    - Updated UserDelegationService to populate new fields from the entity, improving data representation for user delegations.

--- Project.User.Services/DTO/UserDelegationDto.cs
+++ Project.User.Services/DTO/UserDelegationDto.cs
@@ -8,10 +8,16 @@ namespace Ettad.User.Services.DTO
         public string DelegatorUserId { get; set; }
         public string DelegatorUserName { get; set; }
         public string DelegatorFullName { get; set; }
+        public string DelegatorFullNameEn { get; set; }
+        public string DelegatorFullNameAr { get; set; }
+        public string DelegatorMilitaryId { get; set; }
         
         public string DelegateeUserId { get; set; }
         public string DelegateeUserName { get; set; }
         public string DelegateeFullName { get; set; }
+        public string DelegateeFullNameEn { get; set; }
+        public string DelegateeFullNameAr { get; set; }
+        public string DelegateeMilitaryId { get; set; }
 
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
