120ca1d3a2819373ecdd340bb22793590dba0944
eashaanjum <easha.tech@floragroup.net>
2026-06-21 08:52:24 +0300

    Added maximum stock migration

---
+++ Project.EntityFramework/Migrations/20260621055201_AddMaximumStockInAssets.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ettad.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMaximumStockInAssets : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "MaximumStock",
+                table: "BaseItems",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaximumStock",
+                table: "BaseItems");
+        }
+    }
+}
